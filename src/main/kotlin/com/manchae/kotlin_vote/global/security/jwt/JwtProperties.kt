4380e476aead948357c7857f222b71303f071206
tlsalsco41122 <tlsalsco41122@gmail.com>
2024-12-23 20:39:23 +0900

    [Fix] :: .gitignore

--- src/main/kotlin/com/manchae/kotlin_vote/global/security/jwt/JwtProperties.kt
+++ src/main/kotlin/com/manchae/kotlin_vote/global/security/jwt/JwtProperties.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@ConfigurationProperties(prefix = "jwt")
+@ConfigurationProperties(prefix = "application.jwt")
 data class JwtProperties(
     val secretKey: String,
     val accessExp: Long,
