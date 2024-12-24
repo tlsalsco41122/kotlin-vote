9ed4dd343b9db71a3dc1721a376a0af8bed66f10
tlsalsco41122 <tlsalsco41122@gmail.com>
2024-12-24 19:33:30 +0900

    [Feat] :: AuthDetailsService

---
+++ src/main/kotlin/com/manchae/kotlin_vote/global/security/SecurityConfig.kt
@@ -0,0 +1,7 @@
+package com.manchae.kotlin_vote.global.security
+
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SecurityConfig {
+}
--- src/main/kotlin/com/manchae/kotlin_vote/global/security/auth/AuthDetailsService.kt
+++ src/main/kotlin/com/manchae/kotlin_vote/global/security/auth/AuthDetailsService.kt
@@ -1,6 +1,7 @@
 package com.manchae.kotlin_vote.global.security.auth
 
 import com.manchae.kotlin_vote.domain.auth.domain.AuthRepository
+import com.manchae.kotlin_vote.domain.auth.domain.mapper.UserMapper
 import com.manchae.kotlin_vote.domain.auth.exception.UserErrorCode
 import com.manchae.kotlin_vote.global.exception.CustomException
 import org.springframework.security.core.userdetails.UserDetails
@@ -10,12 +11,17 @@ import org.springframework.transaction.annotation.Transactional
 
 @Service
 class AuthDetailsService(
-    private val authRepository: AuthRepository
+    private val authRepository: AuthRepository,
+    private val userMapper: UserMapper
 ): UserDetailsService {
 
     @Transactional(readOnly = true)
     override fun loadUserByUsername(email: String): UserDetails {
-        return authRepository.findByEmail(email)
-            ?: throw CustomException(UserErrorCode.USER_NOT_FOUND)
+        return AuthDetails(
+            user = userMapper.toDomain(
+                entity = authRepository.findByEmail(email)
+                    ?: throw CustomException(UserErrorCode.USER_NOT_FOUND)
+            )
+        )
     }
 }
