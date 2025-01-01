279cdb89fbc503e1a0145ff360ed00e2d15b726a
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-01-01 20:30:22 +0900

    [Feat] :: JwtFilter, SecurityConfig, SpringConfig

--- src/main/kotlin/com/manchae/kotlin_vote/global/security/SecurityConfig.kt
+++ src/main/kotlin/com/manchae/kotlin_vote/global/security/SecurityConfig.kt
@@ -1,7 +1,68 @@
 package com.manchae.kotlin_vote.global.security
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.manchae.kotlin_vote.global.security.jwt.JwtFilter
+import com.manchae.kotlin_vote.global.security.jwt.JwtUtil
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.HttpStatusEntryPoint
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
-class SecurityConfig {
+@EnableWebSecurity
+class SecurityConfig(
+    private val jwtUtil: JwtUtil,
+    private val objectMapper: ObjectMapper
+) {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .cors {
+                it.configurationSource(corsConfigurationSource())
+            }
+            .csrf {
+                it.disable()
+            }
+            .formLogin {
+                it.disable()
+            }
+            .sessionManagement {
+                it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            }
+            .authorizeHttpRequests {
+                it
+                    .requestMatchers("/auth/**").permitAll()
+                    .requestMatchers("/room/**").authenticated()
+                    .requestMatchers("/votes/**").authenticated()
+                    .anyRequest().permitAll() // 그 외 모든 요청은 허용
+            }
+            .addFilterBefore(JwtFilter(jwtUtil, objectMapper), UsernamePasswordAuthenticationFilter::class.java) // JWT 필터 추가
+            .exceptionHandling {
+                it.authenticationEntryPoint(HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)) // 인증 실패 시 401 응답
+            }
+            .build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val corsConfiguration = CorsConfiguration()
+        corsConfiguration.addAllowedOriginPattern("*")
+        corsConfiguration.addAllowedHeader("*")
+        corsConfiguration.addAllowedMethod("*")
+        corsConfiguration.allowCredentials = true
+
+        val urlBasedCorsConfigurationSource = UrlBasedCorsConfigurationSource()
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration)
+
+        return urlBasedCorsConfigurationSource
+    }
 }
