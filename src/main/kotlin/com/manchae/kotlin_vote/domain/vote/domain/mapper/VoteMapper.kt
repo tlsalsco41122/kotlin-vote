a6fa0798f923c4090efcdeab93c353b88b77a056
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-01-01 22:17:14 +0900

    [Bug] :: /votes/{roomId} 400error

--- src/main/kotlin/com/manchae/kotlin_vote/domain/vote/domain/mapper/VoteMapper.kt
+++ src/main/kotlin/com/manchae/kotlin_vote/domain/vote/domain/mapper/VoteMapper.kt
@@ -3,18 +3,10 @@ package com.manchae.kotlin_vote.domain.vote.domain.mapper
 import com.manchae.kotlin_vote.domain.room.domain.entity.RoomEntity
 import com.manchae.kotlin_vote.domain.vote.domain.entity.VoteEntity
 import com.manchae.kotlin_vote.domain.vote.presentation.dto.request.VoteReq
-import com.manchae.kotlin_vote.domain.vote.presentation.dto.response.VoteRes
 import org.springframework.stereotype.Component
 
 @Component
 class VoteMapper {
-
-    fun toResponse(entity: VoteEntity): VoteRes {
-        return VoteRes(
-            id = entity.id,
-            choice = entity.choice
-        )
-    }
 
     fun toEntity(voteReq: VoteReq, room: RoomEntity): VoteEntity {
         return VoteEntity(
--- src/main/kotlin/com/manchae/kotlin_vote/domain/vote/presentation/dto/request/VoteReq.kt
+++ src/main/kotlin/com/manchae/kotlin_vote/domain/vote/presentation/dto/request/VoteReq.kt
@@ -1,5 +1,14 @@
 package com.manchae.kotlin_vote.domain.vote.presentation.dto.request
 
-data class VoteReq(
-    val choice: Boolean
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
+// 이것들을 추가하니 에러가 해결됨...;;;;
+
+/**
+ * @JsonCreator: JSON 을 객체로 변환할 때 사용할 생성자를 지정합니다.
+ * @JsonProperty("choice"): JSON 의 choice 필드를 VoteReq 클래스의 choice 프로퍼티에 매핑합니다.
+ */
+
+data class VoteReq @JsonCreator constructor(
+    @JsonProperty("choice") val choice: Boolean
 )
